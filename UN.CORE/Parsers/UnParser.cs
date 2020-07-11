6a016cb9623b5a7d86c00da5c880d6196e6108b2
antsingle <antsingle@163.com>
2020-07-11 16:04:48 +0800

    init

---
+++ UN.CORE/Parsers/UnParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UN.Core.Parsers
+{
+    public class UnParser
+    {
+        private char[] _cmdTextCharArray;
+        //private int _cmdTextLens = 0;
+        private int _pos = 0;
+        private string _ch;
+
+        public UnParser(string cmdText)
+        {
+            _cmdTextCharArray = cmdText.ToCharArray();
+        }
+        private void NextChar()
+        {
+            _pos = _pos + 1;
+            if (_pos < _cmdTextCharArray.Length)
+                _ch = _cmdTextCharArray[_pos].ToString();
+            else
+                _ch = "";
+            
+        }
+        public void NextToken()
+        {
+            
+        }
+    }
+    public class UnToken
+    {
+        public UnTokenId Id { get; set; }
+        public string Text { get; set; }
+        public  int Pos { get; set; }
+    }
+    public enum UnTokenId
+    {
+        Dot,
+        Sharp,
+        Slash,
+
+        VerbFunc
+
+
+    }
+
+}
